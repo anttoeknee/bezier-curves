831aaa75743739a37f391876e3153dc9fed40115
Anthony Armstrong <anthonym.armstrong@gmail.com>
2025-09-03 20:58:10 +0100

    Implemented memory usage for windows

--- src/core/utils/Mem.cpp
+++ src/core/utils/Mem.cpp
@@ -1,11 +1,19 @@
 #ifdef _WIN32
 
 #include "Mem.hpp"
-
+#include <windows.h>
 #include <string>
+#include <psapi.h>
 
 std::string core::utils::getMemUsage() {
-    return "Windows";
+
+    PROCESS_MEMORY_COUNTERS_EX pmc;
+    if (GetProcessMemoryInfo(GetCurrentProcess(), reinterpret_cast<PROCESS_MEMORY_COUNTERS *>(&pmc), sizeof(pmc))) {
+        SIZE_T physMemUsed = pmc.WorkingSetSize;
+        physMemUsed /= (1024 * 1024); // Convert bytes to MB
+        return std::to_string(physMemUsed) + " MB";
+    }
+    return "N/A";
 }
 
 #endif
